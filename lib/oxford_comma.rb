d1e86a61041223640ed75b543580a2d4bc9e5dd1
Warda Ayaz <wayaz107@gmail.com>
2020-07-14 23:26:24 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
-def oxford_comma(array)
- return array.join("and") if array.size < 3
-    array[-1] = "and" + array[-1]
-    array.join(",")
+def oxford_comma(list)
+ return list.join("and") if list.size < 3
+    list[-1] = "and" + list[-1]
+    list.join(",")
   end
 end
