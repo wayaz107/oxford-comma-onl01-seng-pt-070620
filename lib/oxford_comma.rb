2db4dc97a9790b9cf8d44e1d778fced908ad24b8
Warda Ayaz <wayaz107@gmail.com>
2020-07-14 23:27:31 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,12 @@
 def oxford_comma(list)
  return list.join("and") if list.size < 3
-    list[-1] = "and" + list[-1]
-    list.join(",")
+    list[-1] = "and " + list[-1]
+    list.join(", ")
   end
 end
+
+# def oxford_comma(list)
+#   return list.join(' and ') if list.size < 3
+#   list[-1] = "and " + list[-1]
+#   list.join(', ')
+# end
